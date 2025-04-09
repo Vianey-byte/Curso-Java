b0ce1ccc9858265a53d81c2ea02ff1090a3d0270
Vianey-byte <vianeysorola@gmail.com>
2025-04-08 18:50:53 -0600

    Add files via upload

---
+++ Hospitales/Lena.java
@@ -0,0 +1,14 @@
+package Hospitales;
+
+public class Lena{
+    public static void main (String[] args){
+
+        Pacientes Lena= new Pacientes();
+
+        Lena.Nombre= "Lena Marie";
+        Lena.Edad=25;
+        Lena.Expediente="EXP202409";
+        
+        Lena.mostrarInformacion();
+    }
+}
